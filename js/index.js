1d48cc9d24d57aca30880bf88030e5244f9c5a55
thim25 <thythim2004@gmail.com>
2024-02-12 09:35:07 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -8,12 +8,6 @@ document.getElementById("btnNo").onclick = function () {
   document.getElementById("btnYes").style.fontSize = fS + "px";
 };
 
-// document.getElementById("btnYes").onclick = function () {
-//   document.getElementById("myImage").src = "/image/img2.gif";
-//   document.getElementById("myImage").src = "/videos/babe.mp4";
-//   document.getElementById("myText").innerText = "I love you too❤️🫶";
-// };
-
 document.getElementById("btnYes").onclick = function () {
   var video = document.getElementById("myVideo");
   video.style.display = "block"; // Show the video element
