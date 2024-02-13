e5971139c345852a91f8d6570c27f87de7fe7383
Tim <thythim2004@gmail.com>
2024-02-13 22:21:53 +0700

    update all

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let count = 100;
 let fS = 10;
-document.getElementById("btnNo").onclick = function() {
+document.getElementById("btnNo").onclick = function () {
   count += 20;
   fS += 10;
   document.getElementById("btnYes").style.width = count + "px";
@@ -8,12 +8,13 @@ document.getElementById("btnNo").onclick = function() {
   document.getElementById("btnYes").style.fontSize = fS + "px";
 };
 
-document.getElementById("btnYes").onclick = function() {
+document.getElementById("btnYes").onclick = function () {
   var video = document.getElementById("myVideo");
   video.style.display = "block";
   video.play();
   document.getElementById("myText").innerText = "I love you too❤️🫶";
   document.getElementById("myImage").style.display = "none";
   document.getElementById("btnYes").style.display = "none";
+  document.getElementById("btnYess").style.display = "block";
   document.getElementById("btnNo").style.display = "none";
 };
